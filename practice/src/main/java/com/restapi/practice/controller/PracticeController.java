ce01abb1d1d4df59a08e645d2d23b553ada7a9b2
Sai Ganesh <gsai9960@gmail.com>
2024-03-24 06:46:41 +0530

    Rest code

---
+++ practice/src/main/java/com/restapi/practice/controller/PracticeController.java
@@ -0,0 +1,15 @@
+package com.restapi.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PracticeController {
+	
+	
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
