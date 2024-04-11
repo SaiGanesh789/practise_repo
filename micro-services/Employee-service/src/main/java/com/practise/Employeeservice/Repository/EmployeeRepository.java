6387d5ba92c9a2fa087f61f17473a8db1bc2eb40
SaiGanesh789 <97168604+SaiGanesh789@users.noreply.github.com>
2024-04-12 03:43:00 +0530

    Delete micro-services directory

--- micro-services/Employee-service/src/main/java/com/practise/Employeeservice/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practise.Employeeservice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.practise.Employeeservice.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
